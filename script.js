b38b63ec8af6f03636bf71dcd17f287022419255
Toomun <124077335+Toomun@users.noreply.github.com>
2025-01-07 20:55:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+    abrir.style.display = "none";
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+    abrir.style.display = "block";
+})
